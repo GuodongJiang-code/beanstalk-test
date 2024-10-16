8238387c66395a2e90c1ed7e655f4c0e2f7ff115
Vivi J <guodong2404@gmail.com>
2024-10-16 10:11:31 +0800

    CORS

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { configService } from './utils/config.util';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = configService.getPort();
+  const configService = app.get(ConfigService);
+
+  const allowedOrigins = configService.get<string>('ALLOWED_ORIGINS').split(',').map(origin => origin.trim());
+
+  // Enable CORS
+  app.enableCors({
+    origin: (origin, callback) => {
+      // Check if the incoming origin is in the allowed list
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true); // Allow the origin
+      } else {
+        callback(new Error('Not allowed by CORS')); // Reject the origin
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  const port = configService.get<number>('PORT') || 3000;
   await app.listen(port);
+
   console.log(`Application is running on: ${await app.getUrl()}`);
-  console.log(`Test env: ${configService.get('DB_NAME')}`);
 }
 bootstrap();
